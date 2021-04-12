cd25961bff7681f5e9685af74bbd709a2917dab8
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2021-04-12 11:36:17 +0300

    chore(logs): update logging info

--- run_docker.sh
+++ run_docker.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/env bash
 
-# Change this to match your docker registry
-REGISTRY=${DOCKER_REGISTRY:-thelusina}
-IMAGE=${DOCKER_IMAGE:-housing-prediction-api}
-TAG=${DOCKER_TAG:-latest}
-
-docker build -t "${REGISTRY}"/"${IMAGE}":"${TAG}" .
+./build_docker.sh
 
 docker images
 
